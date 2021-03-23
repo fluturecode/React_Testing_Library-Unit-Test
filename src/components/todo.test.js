b761bf516794339bb12744cbfeb1807dbaa34e74
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-23 19:32:48 -0400

    added todo.test.js;

--- src/components/todo.test.js
+++ src/components/todo.test.js
@@ -1,3 +1,9 @@
-test("test", () => {
-	expect(true).toBe(true)
+import { render, screen } from "@testing-library/react"
+import Todo from "./todo.js"
+
+test("should render todo component", () => {
+	render(<Todo />)
+	const todoElement = screen.getByTestId("todo-1")
+	expect(todoElement).toBeInTheDocument()
+	expect(todoElement).toHaveTextContent("Hello World")
 })
