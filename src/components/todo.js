1f690aff3d14cde1ddad19509608128df543bbdf
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-23 21:59:25 -0400

    added unit tests to todo.test.js

--- src/components/todo.js
+++ src/components/todo.js
@@ -4,7 +4,7 @@ function Todo({ todo }) {
 	const { id, title, completed } = todo
 	const h1 = <h1>{title}</h1>
 	const text = completed ? <strike>{h1}</strike> : h1
-	return <div data-testid={`todod-${id}`}> {text} </div>
+	return <div data-testid={`todo-${id}`}> {text} </div>
 }
 
 export default Todo
--- src/components/todo.test.js
+++ src/components/todo.test.js
@@ -1,9 +1,24 @@
-import { render, screen } from "@testing-library/react"
+import { cleanup, render, screen } from "@testing-library/react"
 import Todo from "./todo.js"
 
-test("should render todo component", () => {
-	render(<Todo />)
+afterEach(() => {
+	cleanup()
+})
+
+test("should render non-completed todo", () => {
+	const todo = { id: 1, title: "wash dishes", completed: false }
+	render(<Todo todo={todo} />)
 	const todoElement = screen.getByTestId("todo-1")
 	expect(todoElement).toBeInTheDocument()
-	expect(todoElement).toHaveTextContent("Hello World")
+	expect(todoElement).toHaveTextContent("wash dishes")
+	expect(todoElement).not.toContainHTML("<strike>")
+})
+
+test("should render completed todo", () => {
+	const todo = { id: 2, title: "grocery shop", completed: true }
+	render(<Todo todo={todo} />)
+	const todoElement = screen.getByTestId("todo-2")
+	expect(todoElement).toBeInTheDocument()
+	expect(todoElement).toHaveTextContent("grocery shop")
+	expect(todoElement).toContainHTML("<strike>")
 })
